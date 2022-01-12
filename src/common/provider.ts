aa2e953816de66ad62ad4009e2bc4db9a3bbf3f5
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-01-12 13:45:15 +0900

    delete comment

--- src/common/provider.ts
+++ src/common/provider.ts
@@ -6,7 +6,3 @@ export const getL2Provider = (network: NetworkName): providers.BaseProvider => {
 	const endpoint = process.env[`KHAOS_${keyName}_JSON_RPC_L2`]
 	return new ethers.providers.JsonRpcProvider(endpoint)
 }
-
-// TODO
-// KHAOS_POLYGON_MAINNET_JSON_RPC_L2
-// KHAOS_POLYGON_MUMBAI_JSON_RPC_L2
